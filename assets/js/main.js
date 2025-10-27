1b99982267cb641227bfbdd61eee839430f1699d
calogerosorce <calogerosorce00@hotmail.com>
2025-10-27 16:59:20 +0100

    Refactor CSS and JavaScript for improved layout and structure, enhancing responsiveness and organization of photo display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,26 @@
 const api = 'https://lanciweb.github.io/demo/api/pictures/'
 const rowEl = document.getElementById('row')
 axios.get(api)
-    .then(res => {
-        const { url, date, title } = res.data
-        const dat = res.data
-        console.log(dat);
+  .then(res => {
+    const { url, date, title } = res.data
+    const dat = res.data
+    console.log(dat);
 
-        rowEl.innerHTML = dat.map(element => {
-            const { url, date, title } = element
-            return `
+    rowEl.innerHTML = dat.map(element => {
+      const { url, date, title } = element
+      return `
         <div class="col">
           <div class="card">
-            <img class="pin" src="./assets/img/pin.svg" alt="">
-            <img class="img_estate" src="${url}" alt="${title || ''}">
-            <p class="gray">${date || ''}</p>
-            <p><strong>${title || ''}</strong></p>
+            <div class="viaggio"> 
+              <img class="pin" src="./assets/img/pin.svg" alt="">
+              <img class="img_estate" src="${url}" alt="${title || ''}">
+            </div>
+            <div class="paragraph"> 
+              <p class="gray">${date || ''}</p>
+              <p><strong>${title || ''}</strong></p>
+            </div>
           </div>
         </div>
       `})
 
-    })
+  })
